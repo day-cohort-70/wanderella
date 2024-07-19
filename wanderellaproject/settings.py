328d6dcf3e29888f8e9498ed8d78b0d38fb0206e
Steve Brownlee <chortlehoort@gmail.com>
2024-07-19 09:26:44 -0500

    Remote route from domain for whitelist of deployed client

--- wanderellaproject/settings.py
+++ wanderellaproject/settings.py
@@ -48,7 +48,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://stevebrownlee.github.io/wanderella-client/'
+    'https://stevebrownlee.github.io'
 )
 
 
